721b37577740cc0d5e4ccbf23324699866972675
rishibakshi <guitarguyrishi@gmail.com>
2024-04-19 01:11:43 +0530

    feat: Add Request interface and status enum

---
+++ backend/src/enums/request.enum.ts
@@ -0,0 +1,5 @@
+export enum RequestStatus {
+    Pending,
+    Accepted,
+    Rejected
+}
---
+++ backend/src/interfaces/request.interface.ts
@@ -0,0 +1,8 @@
+import { Types } from "mongoose"
+import type { RequestStatus } from "../enums/request.enum.js"
+
+export interface IRequest {
+    sender:Types.ObjectId
+    receiver:Types.ObjectId
+    status:RequestStatus
+}

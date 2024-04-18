05b0a6cbf92d43c638449197a50027c42a869248
rishibakshi <guitarguyrishi@gmail.com>
2024-04-19 00:40:50 +0530

    feat: Implement basic express server and integrate socket.io

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,17 @@
- 
+import express, { Request, Response, json } from 'express'
+import {createServer} from 'http'
+import { Server } from 'socket.io'
+
+
+const app=express()
+const server=createServer(app)
+const io=new Server(server,{cors:{origin:"*"}})
+
+
+app.get("/",(req:Request,res:Response)=>{
+    res.status(200).json({running:true})
+})
+
+server.listen(8000,()=>{
+    console.log('server [STARTED] ~ http://localhost:8000');
+})

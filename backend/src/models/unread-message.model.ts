219785ffe3679b626cfbe961d1aa3d500534cfc6
rishibakshi <guitarguyrishi@gmail.com>
2024-04-19 01:55:27 +0530

    feat: Add Unread-Message model

---
+++ backend/src/models/unread-message.model.ts
@@ -0,0 +1,25 @@
+import { Schema, model } from "mongoose";
+import type { IUnreadMessage } from "../interfaces/unread-message.interface.js";
+
+
+const unreadMessageSchema = new Schema<IUnreadMessage>({
+    user:{
+        type:Schema.ObjectId,
+        ref:"User",
+        required:true
+    },
+    chat:{
+        type:Schema.ObjectId,
+        ref:"Chat",
+        required:true
+    },
+    count:{
+        type:Number,
+        required:true
+    },
+    readAt:{
+        type:Date,
+    },
+},{versionKey:false,timestamps:true})
+
+export const UnreadMessage = model<IUnreadMessage>("UnreadMessage",unreadMessageSchema)

75bffb0bcf73d8689a78c8d4e6af5b787234aa25
rishibakshi <guitarguyrishi@gmail.com>
2024-04-19 13:13:54 +0530

    feat: add middlewares

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,20 @@
 import express, { Request, Response, json } from 'express'
 import {createServer} from 'http'
 import { Server } from 'socket.io'
+import cors from 'cors'
+import morgan from 'morgan'
+import cookieParser from 'cookie-parser'
 
 
 const app=express()
 const server=createServer(app)
 const io=new Server(server,{cors:{origin:"*"}})
+
+// middlewares
+app.use(cors({credentials:true,origin:""}))
+app.use(express.json())
+app.use(cookieParser())
+app.use(morgan('tiny'))
 
 
 app.get("/",(req:Request,res:Response)=>{

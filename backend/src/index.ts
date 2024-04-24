fdfa580aa4c723a7c60992856650de1f2782c33d
rishibakshi <guitarguyrishi@gmail.com>
2024-04-24 13:41:47 +0530

    feat: Implement Message event handling in socket

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,6 +9,7 @@ import { errorMiddleware } from './middlewares/error.middleware.js'
 import { connectDB } from './config/db.config.js'
 import { env } from './schemas/env.schema.js'
 import type { AuthenticatedSocket } from './interfaces/authenticated-socket.interface.js'
+import type { IMessage } from './interfaces/message.interface.js'
 
 import authRoutes from './routes/auth.router.js'
 import chatRoutes from './routes/chat.router.js'
@@ -16,6 +17,9 @@ import userRoutes from './routes/user.router.js'
 import requestRoutes from './routes/request.router.js'
 
 import { socketAuthenticatorMiddleware } from './middlewares/socket-auth.middleware.js'
+import { Events } from './enums/event.enum.js'
+import { Message } from './models/message.model.js'
+import { UnreadMessage } from './models/unread-message.model.js'
 
 
 const app=express()
@@ -24,6 +28,9 @@ const io=new Server(server,{cors:{credentials:true,origin:'*'}})
 
 // database connection
 connectDB()
+
+// userSocketIds
+const userSocketIds = new Map()
 
 // middlewares
 app.use(cors({credentials:true,origin:"*"}))
@@ -43,7 +50,37 @@ io.use(socketAuthenticatorMiddleware)
 // socket
 io.on("connection",(socket:AuthenticatedSocket)=>{
 
-    console.log(`${socket.user?.name} just connected with socket`);
+    userSocketIds.set(socket.user?._id.toString(),socket.id)
+
+    socket.on(Events.MESSAGE,async({chat,content,attachments,members}:Omit<IMessage , "sender"> & {members : Array<string>})=>{
+
+        // save to db
+        const newMessage = await Message.create({chat,content,attachments,sender:socket.user?._id})
+        
+        // realtime response
+        const memberIds = members.filter(member=>member!==socket.user?._id.toString())
+        const memberSocketsIds = memberIds.map(memberId=>userSocketIds.get(memberId))
+        io.to(memberSocketsIds).emit(Events.MESSAGE,newMessage)
+
+        // unread message creation for receivers
+        const updateOrCreateUnreadMessagePromise = memberIds.map(async(memberId)=>{
+
+            const isExistingUnreadMessage = await UnreadMessage.findOne({chat,user:memberId})
+
+            if(isExistingUnreadMessage){
+                isExistingUnreadMessage.count++
+                isExistingUnreadMessage.save()
+                return isExistingUnreadMessage
+            }
+
+           return UnreadMessage.create({chat,count:1,user:memberId})
+
+        })
+
+        await Promise.all(updateOrCreateUnreadMessagePromise)
+        io.to(memberSocketsIds).emit(Events.UNREAD_MESSAGE,{chat,message:newMessage.content.substring(0,30)})
+
+    })
 
     socket.on("disconnect",()=>{
         console.log(`${socket.user?.name} left`);

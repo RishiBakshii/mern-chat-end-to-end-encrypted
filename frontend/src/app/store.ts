2a7ce700d4bfe79f94067611ce2e74a7f1d63225
rishibakshi <guitarguyrishi@gmail.com>
2024-04-28 12:28:22 +0530

    feat: Setup Redux-Toolkit store and typed hooks

---
+++ frontend/src/app/store.ts
@@ -0,0 +1,12 @@
+import {configureStore} from '@reduxjs/toolkit'
+
+export const store = configureStore({
+    reducer:{
+
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware()
+    .concat(),
+})
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {App} from './App.tsx'
 import './index.css'
+import { Provider } from 'react-redux'
+import { store } from './app/store.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
   </React.StrictMode>,
 )

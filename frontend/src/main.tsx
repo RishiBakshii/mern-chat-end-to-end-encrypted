d74f7fe027acf00276ed119b0313488b4c26ab39
rishibakshi <guitarguyrishi@gmail.com>
2024-04-28 12:31:06 +0530

    feat(react-hot-toast): Add Toaster component at top level

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,10 +4,12 @@ import {App} from './App.tsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import { store } from './app/store.ts'
+import {Toaster} from 'react-hot-toast'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
+        <Toaster/>
         <App />
     </Provider>
   </React.StrictMode>,

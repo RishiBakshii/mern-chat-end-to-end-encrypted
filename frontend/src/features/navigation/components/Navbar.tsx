f58dfcad716553602d038bbd3e46ef5353719f29
rishibakshi <guitarguyrishi@gmail.com>
2024-04-30 01:58:16 +0530

    feat: Add Navbar component

--- frontend/src/features/navigation/components/Navbar.tsx
+++ frontend/src/features/navigation/components/Navbar.tsx
@@ -1,6 +1,14 @@
+import { useAppSelector } from "../../../app/hooks"
+import { selectLoggedInUser } from "../../auth/authSlice"
 
 export const Navbar = () => {
+
+  const loggedInUser = useAppSelector(selectLoggedInUser)
+
   return (
-    <div>Navbar</div>
+    <nav className="flex items-center h-14 justify-around shadow">
+      <h4 className="text-3xl font-bold">Chorus</h4>
+      <img className="h-12 rounded-full aspect-square" src={loggedInUser?.avatar} alt={`${loggedInUser?.name} avatar`} />
+    </nav>
   )
 }

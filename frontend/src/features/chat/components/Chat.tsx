0a1a8b63fef7be96a1a1f64d4cc6106d38820206
rishibakshi <guitarguyrishi@gmail.com>
2024-05-02 02:38:38 +0530

    feat: Move Chat Details into dedicated ChatDetails component

--- frontend/src/features/chat/components/Chat.tsx
+++ frontend/src/features/chat/components/Chat.tsx
@@ -6,6 +6,7 @@ import { useAppSelector } from "../../../app/hooks"
 import { selectSelectedChatId } from "../chatSlice"
 import { useEffect } from "react"
 import { useLazyGetMessagesByChatIdQuery } from "../../messages/api"
+import { ChatDetails } from "./ChatDetails"
 
 export const Chat = () => {
   
@@ -46,18 +47,10 @@ export const Chat = () => {
               <>
                 {/* chat name,info and options */}
                 <div className="flex flex-row justify-between items-center">
-
-                  <div className="flex flex-col gap-y-1">
-                    <h4 className="font-medium text-4xl">Design chat</h4>
-                    <p className="text-gray-500">23 members, 10 online</p>
-                  </div>
-
-                  <div className="flex items-center gap-x-5">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-gray-500"><path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" /></svg>
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-gray-500"><path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" /></svg>
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-gray-500"><path strokeLinecap="round" strokeLinejoin="round" d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z" /></svg>
-                  </div>
-
+                  {
+                    !isFetching && chats && selectedChatId &&  
+                    <ChatDetails chat={chats.find(chat=>chat._id===selectedChatId)!} />
+                  }
                 </div>
 
                 {/* messages area */}

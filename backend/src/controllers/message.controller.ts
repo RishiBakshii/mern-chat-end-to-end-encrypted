2583a5616adce7274a5282cafac5c681c408cad4
rishibakshi <guitarguyrishi@gmail.com>
2024-05-01 19:37:43 +0530

    feat: Implement sender feild population in getMessage controller

--- backend/src/controllers/message.controller.ts
+++ backend/src/controllers/message.controller.ts
@@ -5,7 +5,7 @@ import { asyncErrorHandler } from "../utils/error.utils.js";
 const getMessages = asyncErrorHandler(async(req:Request,res:Response,next:NextFunction)=>{
 
     const {id} = req.params
-    const messages = await Message.find({chat:id})
+    const messages = await Message.find({chat:id}).populate("sender",['username','avatar'])
     return res.status(200).json(messages)
 
 })

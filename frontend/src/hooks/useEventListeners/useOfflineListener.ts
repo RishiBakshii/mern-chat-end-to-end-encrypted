a04bb5c3b4aa67ac578a410a4adac72a84d98c48
rishibakshi <guitarguyrishi@gmail.com>
2024-05-17 20:55:06 +0530

    feat: Implement manual cache update of friends isActive status in useOfflineListener custom hook

--- frontend/src/hooks/useEventListeners/useOfflineListener.ts
+++ frontend/src/hooks/useEventListeners/useOfflineListener.ts
@@ -1,9 +1,23 @@
 import { Events } from "../../enums/events"
+import { friendApi } from "../../services/api/friendApi"
+import { useAppDispatch } from "../../services/redux/store/hooks"
 import { useSocketEvent } from "../useSocket/useSocketEvent"
 
 export const useOfflineListener = () => {
 
+    const dispatch = useAppDispatch()
+
     useSocketEvent(Events.OFFLINE,(userId:string)=>{
-        console.log(`${userId} is offline`)
-      })
+
+      dispatch(
+        friendApi.util.updateQueryData('getFriends',undefined,(draft)=>{
+          const friend = draft.find(draft=>draft._id===userId)
+
+          if(friend){
+            friend.isActive = false
+          }
+        })
+      )
+
+    })
 }

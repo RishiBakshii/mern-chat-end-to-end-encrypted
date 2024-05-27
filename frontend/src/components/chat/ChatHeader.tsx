30d22c7e940e97dd165625ce7685c1673e630006
rishibakshi <guitarguyrishi@gmail.com>
2024-05-27 15:46:36 +0530

    fix: Update chatHeader component to only toggleChatDetails bar when 2xl breakpoint is hit

--- frontend/src/components/chat/ChatHeader.tsx
+++ frontend/src/components/chat/ChatHeader.tsx
@@ -1,3 +1,4 @@
+import { useMediaQuery } from "../../hooks/useUtils/useMediaQuery"
 import { ChatHeaderOptions } from "./ChatHeaderOptions"
 
 type PropTypes = {
@@ -9,11 +10,13 @@ type PropTypes = {
 }
 
 export const ChatHeader = ({totalMembers,chatName,isGroupChat,openRemoveMemberForm,toggleChatDetailsBar}:PropTypes) => {
+  
+  const is2xl = useMediaQuery(1536)
 
   return (
     <div className="flex items-center justify-between text-text bg-background">
 
-        <div onClick={toggleChatDetailsBar} className="flex flex-col gap-y-1">
+        <div onClick={()=>is2xl?toggleChatDetailsBar():""} className="flex flex-col gap-y-1">
             <h4 className="font-medium text-4xl text-fluid-h4">{chatName}</h4>
             <p className="text-secondary-darker text-fluid-p">{totalMembers} Members</p>
         </div>

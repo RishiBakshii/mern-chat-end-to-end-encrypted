2d0357a793693ba04b5ebcbed16c031f2d028240
rishibakshi <guitarguyrishi@gmail.com>
2024-06-05 00:38:11 +0530

    refactor: Remove callIn button from MessageInput component

--- frontend/src/components/ui/MessageInput.tsx
+++ frontend/src/components/ui/MessageInput.tsx
@@ -13,12 +13,12 @@ export const MessageInput = ({ messageVal, setMessageVal,toggleCallInForm,toggle
   return (
     <div className="flex bg-secondary rounded-xl text-text">
       
-      <button type="button" onClick={toggleCallInForm}>
+      {/* <button type="button" onClick={toggleCallInForm}>
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6 9 12.75l4.286-4.286a11.948 11.948 0 0 1 4.306 6.43l.776 2.898m0 0 3.182-5.511m-3.182 5.51-5.511-3.181" />
 </svg>
 
-      </button>
+      </button> */}
 
       <div className="px-3 py-4 justify-center items-center flex relative ">
           <svg

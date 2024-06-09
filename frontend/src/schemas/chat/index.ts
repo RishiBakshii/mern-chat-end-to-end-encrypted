0f96050f6a3be823d143b9829090ddff3cc42795
rishibakshi <guitarguyrishi@gmail.com>
2024-06-09 15:23:42 +0530

    refactor: Update groupChat zod validation schema

--- frontend/src/schemas/chat/index.ts
+++ frontend/src/schemas/chat/index.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 const groupChatSchema = z.object({
     name:z.string({required_error:"Name is required"})
     .min(3,{message:"Name must be minimum of 3 characters"})
-    .max(25,{message:"Name cannot be more than 30 characters"}), 
+    .max(20,{message:"Name cannot be more than 20 characters"}), 
 })
 
 

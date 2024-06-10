470d3001372bccf1861ad562b0a52cecf7b6d25e
rishibakshi <guitarguyrishi@gmail.com>
2024-06-11 00:55:15 +0530

    fix: socket authentication middleware cookie extraction logic

--- backend/src/middlewares/socket-auth.middleware.ts
+++ backend/src/middlewares/socket-auth.middleware.ts
@@ -1,7 +1,7 @@
 import { NextFunction } from "connect";
+import cookie from 'cookie';
 import jwt from 'jsonwebtoken';
-import type { AuthenticatedSocket } from "../interfaces/auth/auth.interface.js";
-import type { IUser } from "../interfaces/auth/auth.interface.js";
+import type { AuthenticatedSocket, IUser } from "../interfaces/auth/auth.interface.js";
 import { User } from "../models/user.model.js";
 import { env } from "../schemas/env.schema.js";
 import { CustomError } from "../utils/error.utils.js";
@@ -9,28 +9,37 @@ import { CustomError } from "../utils/error.utils.js";
 export const socketAuthenticatorMiddleware = async(socket:AuthenticatedSocket,next:NextFunction)=>{
 
     try {
-        const token = socket.handshake.headers.cookie?.split(";")[0].split("=")[1]
-    
-        if(!token){
-            return next(new CustomError("Token missing, please login again",401))
-        }
-    
-        const decodedInfo=jwt.verify(token,env.JWT_SECRET) as IUser['_id']
-    
-        if(!decodedInfo || !decodedInfo._id){
-            return next(new CustomError("Invalid token please login again",401))
-        }
-    
-        const existingUser = await User.findOne({_id:decodedInfo._id})
-    
-        if(!existingUser){
-            return next(new CustomError('Invalid Token, please login again',401))
-        }
-    
-        socket.user=existingUser
-        next()
+
+            const cookies = socket.handshake.headers.cookie;
+
+            if (!cookies) {
+                return next(new CustomError("Token missing, please login again", 401));
+            } 
+
+            const parsedCookies = cookie.parse(cookies)
+            const token = parsedCookies['token']
         
-    } catch (error) {
+            if(!token){
+                return next(new CustomError("Token missing, please login again",401))
+            }
+        
+            const decodedInfo=jwt.verify(token,env.JWT_SECRET) as IUser['_id']
+        
+            if(!decodedInfo || !decodedInfo._id){
+                return next(new CustomError("Invalid token please login again",401))
+            }
+        
+            const existingUser = await User.findOne({_id:decodedInfo._id})
+        
+            if(!existingUser){
+                return next(new CustomError('Invalid Token, please login again',401))
+            }
+        
+            socket.user=existingUser
+            next()
+        }
+        
+    catch (error) {
         console.log(error);
         return next(new CustomError("Invalid Token, please login again", 401))
     }

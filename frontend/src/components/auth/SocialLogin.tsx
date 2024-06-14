30faab6ae21f662912141463e9a452f1a6c39f4a
rishibakshi <guitarguyrishi@gmail.com>
2024-06-15 02:30:18 +0530

    fix: Anchor tag positioning in SocialLogin component

--- frontend/src/components/auth/SocialLogin.tsx
+++ frontend/src/components/auth/SocialLogin.tsx
@@ -8,15 +8,11 @@ type PropTypes = {
 export const SocialLogin = ({googleLink}:PropTypes) => {
 
   return (
-    <div className="flex items-center gap-x-2 gap-3 max-md:flex-wrap">
-        <button className="px-6 py-2 outline outline-1 rounded-sm outline-secondary-dark flex items-center gap-x-2 w-full">
-            <img src={googlePng} className="w-7" alt="google" />
-            <a href={googleLink}><p className="text-fluid-p">Continue with google</p></a>
-        </button>
-        {/* <button className="px-6 py-2 outline outline-1 rounded-sm  outline-secondary-dark flex items-center gap-x-2 w-full">
-            <img src={githubPng} className="w-7 scale-[1.5] invert" alt="github" />
-            <a href={githubLink} ><p className="text-fluid-p">Continue with github</p></a>
-        </button> */}
-    </div>
+    <a href={googleLink} className="w-full">
+      <button className="px-6 py-2 outline outline-1 rounded-sm outline-secondary-dark hover:outline hover:outline-1 hover:outline-primary/50 flex items-center gap-x-2 w-full">
+          <img src={googlePng} className="w-7" alt="google" />
+          <p className="text-fluid-p">Continue with google</p>
+      </button>
+    </a>
   )
 }

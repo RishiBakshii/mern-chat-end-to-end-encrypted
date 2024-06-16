c3487dbef265fdf87c649e7093bf0918ff9a4e46
rishibakshi <guitarguyrishi@gmail.com>
2024-06-16 19:05:25 +0530

    fix: UnreadMessages from another chat getting pushed in the selected chat

--- frontend/src/hooks/useEventListeners/useMessageListener.ts
+++ frontend/src/hooks/useEventListeners/useMessageListener.ts
@@ -1,28 +1,24 @@
 import { Events } from "../../enums/events"
 import type { IMessage } from "../../interfaces/messages"
 import { messageApi } from "../../services/api/messageApi"
-import { selectSelectedChatDetails } from "../../services/redux/slices/chatSlice"
-import { useAppDispatch, useAppSelector } from "../../services/redux/store/hooks"
+import { useAppDispatch } from "../../services/redux/store/hooks"
 import { useSocketEvent } from "../useSocket/useSocketEvent"
 
 export const useMessageListener = () => {    
 
-    const selectedChatDetails = useAppSelector(selectSelectedChatDetails)
+  
     const dispatch = useAppDispatch()
 
     useSocketEvent(Events.MESSAGE,async(newMessage:IMessage)=>{
-      
-        if(selectedChatDetails){
 
           dispatch(
-            messageApi.util.updateQueryData('getMessagesByChatId',{_id:selectedChatDetails._id,page:1},(draft)=>{
+            messageApi.util.updateQueryData('getMessagesByChatId',{_id:newMessage.chat,page:1},(draft)=>{
               draft.messages.push(newMessage)
               if(!draft.totalPages){
                 draft.totalPages =1
               }
             })
           )
-        }
-        
+
       })
 }
